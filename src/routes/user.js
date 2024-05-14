3a3153feef9bc8f69b5b2d93101577c1a6fed89e
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-05-14 09:08:59 -0300

    User routes created

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const UserController = require('../controller/userController')
 const router = express.Router();
 
-
+router
+    .post('/', UserController.login)
+    .post('/register', UserController.register)
 
 module.exports = router;
