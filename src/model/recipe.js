7a85b557bd99710f33bcd9c6b87c96e3240975f7
Felipe Vieira <127983939+felipevieirafmv@users.noreply.github.com>
2024-05-21 10:18:16 -0300

    Update recipe.js

--- src/model/recipe.js
+++ src/model/recipe.js
@@ -1,36 +1,38 @@
 const mongoose = require('mongoose');
-const { userSchema } = require('./user');
 
 const Recipe = mongoose.model('Recipe',
     new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minlength: 3
-    },
-    description: {
-        type: String,
-        required: true,
-        minlength: 10
-    },
-    ingredients: [
-        {
+        title: {
+            type: String,
+            required: true,
+            minlength: 3
+        },
+        description: {
+            type: String,
+            required: true,
+            minlength: 10
+        },
+        ingredients: [
+            {
+                qtd: {
+                    type: String,
+                    required: true
+                },
+                ingredient: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        prepare: [{
             type: String,
             required: true
-        },
-        {
+        }],
+        user: {
             type: String,
             required: true
         }
-    ],
-    prepare: [{
-        type: String,
-        required: true
-    }],
-    user: {
-        type: String,
-        required: true
-    }
-}));
+    })
+);
 
-module.exports = Recipe
+module.exports = Recipe;
